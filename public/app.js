6665a2d4b7206a8a425f12de58e04202f606f08e
yasminachavez <yasminachavez@yahoo.com>
2023-05-23 16:33:26 -0700

    finished activity

--- public/app.js
+++ public/app.js
@@ -4,29 +4,76 @@
 
 // Create map
 
+// Create map:                                                       
+// const myMap = L.map('map', {
+//     center: [48.868672, 2.342130],
+//     zoom: 12,
+// }); replaced with below refactor
 
+const map = L.map('map').setView([48.868672, 2.342130], 12);
 
 
 // Add openstreetmap tiles
 
+// Add OpenStreetMap tiles: //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
-
-//Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 // Create and add a geolocation marker
 
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(map).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
+const circle = L.circle([48.87007, 2.346453], {
+    color: 'blue',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
+// const polygon = L.polygon([
+//     [48.87007, 2.346453],
+//     [48.87007, 2.246],
+//     [48.9, 2.35]
+// ]).addTo(map);
+// ***see below for refactor
 
 // Draw the 2nd arrondissement
-
-
-
-
-// Metro station markers
-
-
-
+let latlngs = [
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]
+]
+const polygon = L.polygon(latlngs,{
+       color: 'purple',
+    //    fill:false ***in order to take away fill
+       fillOpacity: 0.1,
+}).addTo(map);
 
 // Create red pin marker
+const redPin = L.icon({
+    iconUrl: './assets/red-pin.png',
+    iconSize: [38,38],
+    iconAnchor: [19,38]
+})
+
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739], {icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167], {icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543], {icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714], {icon: redPin}).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(map)
+
+
+
+
